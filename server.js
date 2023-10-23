ad4f4a3d4734f40ff0fbb4ef9d9b880399b198a7
“shawn.hassen” <shawn.hassen@zoom.us>
2023-10-23 10:38:26 -0400

    Still working on server stuff

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const fastify = require("fastify")();
+const path = require("path");
+const fs = require("fs");
+
+const publicDir = path.join(__dirname, "public");
+
+// fastify.register(async (instance) => {
+//   instance.addHook("preHandler", (req, res) => {
+//     const filePath = path.join(publicDir, req.url.replace(/^\//, ""));
+//     res.send(filePath);
+//   });
+// });
 
 fastify.get("/", (req, res) => {
-  res.send();
+  res.send({ message: "Hello from fastify!" });
 });
 
-fastify.listen(3000, (err, address) => {
+fastify.listen({ port: 3001 }, (err, addr) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
-
-  console.log(`Server listening on ${address}`);
+  console.log(`Server listening on ${addr}`);
 });
